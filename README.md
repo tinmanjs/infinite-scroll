66130677b129708a2c225924850b27fe0d5ca438
tinmanjs <tinmanjs@protonmail.com>
2020-08-03 00:03:42 -0400

    Display data on html page with Javascript

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Infinite Scroll
 This is an implementation of an infinite scroll.
 
+![](art/banner01.png?raw=true)
+
 Points of Interest Include:
 - Generated loading SVG via [Loading.io](https://loading.io/)
 - Use the [Bebas Neue Google Font](https://fonts.google.com/specimen/Bebas+Neue)
 - Responsive Design with Media Queries
 
-a [tinmanJS](https://tinmanjs.com/) production
+> a [tinmanJS](https://tinmanjs.com/) production
--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-console.log('Template');
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+let photosArray = []
+
+// Unsplash API
+const count = 10
+const apiKey = 'YOUR_KEY'
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach(photo => {
+    // Create <a> element to link to Unsplash
+    const item = document.createElement('a')
+    item.setAttribute('href', photo.links.html)
+    item.setAttribute('target', '_blank')
+    // Create an <img> element for photo
+    const img = document.createElement('img')
+    img.setAttribute('src', photo.urls.regular)
+    img.setAttribute('alt', photo.alt_description)
+    img.setAttribute('title', photo.alt_description)
+    // Put <img> inside <a>, then put both inside imageContainer element
+    item.appendChild(img)
+    imageContainer.appendChild(item)
+  })
+  console.log(photosArray)
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiURL)
+    photosArray = await response.json()
+    displayPhotos()
+  } catch (error) {
+    // Catch error here - If no API_KEY
+    photosArray = [{
+      alt_description: "person holding white printer paper",
+      links: {html: "https://unsplash.com/photos/L7CDWeB2CSk"},
+      urls: {regular: "https://images.unsplash.com/photo-1594392175511-30eca83d51c8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE1NDA4MH0"}
+    },
+    {
+      alt_description: "woman in black shirt with purple hair",
+      links: {html: "https://unsplash.com/photos/L7CDWeB2CSk"},
+      urls: {regular: "https://images.unsplash.com/photo-1596212602090-96a97c24694f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE1NDA4MH0"}
+    }
+    ]
+    photosArray.forEach(photo => {
+      // Create <a> element to link to Unsplash
+      const item = document.createElement('a')
+      item.setAttribute('href', photo.links.html)
+      item.setAttribute('target', '_blank')
+      // Create an <img> element for photo
+      const img = document.createElement('img')
+      img.setAttribute('src', photo.urls.regular)
+      img.setAttribute('alt', photo.alt_description)
+      img.setAttribute('title', photo.alt_description)
+      // Put <img> inside <a>, then put both inside imageContainer element
+      item.appendChild(img)
+      imageContainer.appendChild(item)
+    })
+    displayPhotos()
+  }
+}
+
+// On Load
+getPhotos()
